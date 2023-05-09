89004c1a1a7e6cecfb7632575c9136b2ecb97606
Poonam <poonam.chauhan229@gmail.com>
2023-05-09 10:38:05 +0530

    8th done 9started

---
+++ 09react-slider-app_4thmay/src/Components/LockSlider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LockSlider = () => {
+  return (
+    <div>LockSlider</div>
+  )
+}
+
+export default LockSlider
---
+++ 09react-slider-app_4thmay/src/Components/SlideToUnlock.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SlideToUnlock = () => {
+  return (
+    <div>SlideToUnlock</div>
+  )
+}
+
+export default SlideToUnlock
